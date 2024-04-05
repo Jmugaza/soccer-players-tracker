a13db55c94244fd56e06a16cccbcd7883892ace2
jeilanim <jeilandahiri2@gmail.com>
2024-04-05 17:22:24 -0400

    Implementing OAuth

--- routes/players.js
+++ routes/players.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const playersCtrl = require('../controllers/players');
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let session = require('express-session');
+let passport = require('passport');
 let methodOverride = require('method-override');
 
 
@@ -10,9 +12,11 @@ require('dotenv').config();
 // Require config/database.js
 require('./config/database.js');
 
+require('./config/passport');
+
 
 let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+let playersRouter = require('./routes/players.js');
 
 var app = express();
 
@@ -25,12 +29,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
+
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
 
 // mount method-override middleware
 app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/players', playersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
